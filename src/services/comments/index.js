895844bf2588e9d471c02688e3b457ad45ab129c
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-10-06 20:14:55 +0200

    it works :)

--- src/services/comments/index.js
+++ src/services/comments/index.js
@@ -8,11 +8,11 @@ commentsRouter.post("/:id/comments", async (req, res, next) => {
   try {
     const post = await postsModel.findById(req.params.id);
     if (post) {
-      await postsModel.findByIdAndUpdate(req.params.id, {
+    const newPost =  await postsModel.findByIdAndUpdate(req.params.id, {
         $push: { comments: req.body },
       });
 
-      res.status(204).send();
+      res.status(204).send(newPost);
     } else {
       next(
         createHttpError(404, `post with id ${req.params.id} doesn't exist!!`)
@@ -31,11 +31,30 @@ commentsRouter.get("/:id/comments", async (req, res, next) => {
     next(error);
   }
 });
-commentsRouter.delete("/:id/comments/:postiId", async (req, res, next) => {
+commentsRouter.get("/:id/comments/:postId", async (req, res, next) => {
+    try {
+      const postID = req.params.id;
+      const post = await postsModel.findById(postID);
+      if (post) {
+        const comment = post.comments.find(post=> post._id.toString() === req.params.postId)
+        console.log(comment)
+        if (comment){
+            res.send(comment)
+        } else {
+            next(createHttpError(404, `comment with id ${req.params.postId} doesn't exist!`))
+        }
+      } else {
+         next(createHttpError(404, `post with id ${req.params.id} doesn't exist!` )) 
+      }
+    } catch (error) {
+      next(error);
+    }
+  });
+commentsRouter.delete("/:id/comments/:postId", async (req, res, next) => {
   try {
       const comment = await postsModel.findByIdAndUpdate(
           req.params.id,
-          {$pull: {comments: {_id: req.params.postiId}}},
+          {$pull: {comments: {_id: req.params.postId}}},
           {new: true}
       )
       if (comment){
@@ -47,8 +66,21 @@ commentsRouter.delete("/:id/comments/:postiId", async (req, res, next) => {
     next(error);
   }
 });
-commentsRouter.put("/:id/comments/:id", async (req, res, next) => {
+commentsRouter.put("/:id/comments/:postId", async (req, res, next) => {
   try {
+      const post = await postsModel.findById(req.params.id)
+      if (post) {
+          const index = post.comments.findIndex(p=> p._id.toString() === req.params.postId )
+          if (index !== -1) {
+              post.comments[index]= {...post.comments[index].toObject(), ...req.body}
+              await post.save()
+              res.send(post)
+          } else {
+              next(createHttpError(404, `comment with this ID ${req.params.postId} doesn't exist`))
+          }
+      } else {
+          next(createHttpError(404, `post with this ID ${req.params.id} doesn't exist`))
+      }
   } catch (error) {
     next(error);
   }
